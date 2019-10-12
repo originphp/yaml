eb2e65ac1dbf186a3442a17cc9c89687159c2b5f
Jamiel <20553479+jamielsharief@users.noreply.github.com>
2019-10-12 15:57:25 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,145 @@
+# Yaml
+
+![license](https://img.shields.io/badge/license-MIT-brightGreen.svg)
+[![build](https://travis-ci.org/originphp/yaml.svg?branch=master)](https://travis-ci.org/originphp/yaml)
+[![coverage](https://coveralls.io/repos/github/originphp/yaml/badge.svg?branch=master)](https://coveralls.io/github/originphp/yaml?branch=master)
+
+
+The YAML utility is for reading (parsing) and writing YAML files. 
+
+> The YAML utility does not cover the complete specification, it is designed to read and write configuration files, and data from the database so that it can be read and edited in user friendly way.
+
+## Installation
+
+To install this package
+
+```linux
+$ composer require originphp/yaml
+```
+
+## Create a YAML string
+
+```php
+use Origin\Yaml\Yaml;
+$employees = [
+    ['name'=>'Jim','skills'=>['php','mysql','puppeteer']],
+    ['name'=>'Amy','skills'=>['ruby','ruby on rails']],
+];
+$yaml = Yaml::fromArray($employees );
+```
+
+That will return the following
+
+```yaml
+- name: Jim
+  skills:
+    - php
+    - mysql
+    - puppeteer
+- name: Amy
+  skills:
+    - ruby
+    - ruby on rails
+```
+
+Here is an example converting a record using the Bookmark model
+
+```php
+ $bookmark = $this->Bookmark->get(1,[
+            'associated'=>[
+                'User','Tag'
+                ]
+            ]);
+$string = Yaml::fromArray($bookmark->toArray()); 
+
+```
+
+This will create the following YAML string:
+
+```yaml
+id: 1
+user_id: 1
+title: OriginPHP
+description: The PHP framework for rapidly building scalable web applications.
+url: https://www.originphp.com
+category: Computing
+created: 2018-12-28 15:25:34
+modified: 2019-05-02 13:08:44
+user: 
+  id: 1
+  name: Demo User
+  email: demo@example.com
+  password: $2y$10$/clqxdb.aWe43VXDUn8tA.yxKbWHZT3rN7gqITFaj32PZHI3.DkzW
+  dob: 1999-12-28
+  created: 2018-12-28 15:24:13
+  modified: 2018-12-28 15:24:13
+tags: 
+  - id: 1
+    title: Framework
+    created: 2019-03-07 18:45:43
+    modified: 2019-03-07 18:45:43
+    bookmarksTag: 
+      bookmark_id: 1
+      tag_id: 1
+  - id: 2
+    title: PHP
+    created: 2019-03-07 18:45:43
+    modified: 2019-03-07 18:45:43
+    bookmarksTag: 
+      bookmark_id: 1
+      tag_id: 2
+tag_string: Framework,PHP
+```
+
+## Read YAML
+
+To create an array from a YAML string
+
+```php
+$employee =  'name: Tom
+position: developer
+skills:
+    - php
+    - mysql
+    - js
+addresses:
+    - street: 14 some road
+      city: london
+    - street: 21 some avenue
+      city: leeds
+summary:
+    this is a text description
+    for this employee';
+$array = Yaml::toArray($employee);
+/*
+Array
+(
+    [name] => Tom
+    [position] => developer
+    [skills] => Array
+        (
+            [0] => php
+            [1] => mysql
+            [2] => js
+        )
+
+    [addresses] => Array
+        (
+            [0] => Array
+                (
+                    [street] => 14 some road
+                    [city] => london
+                )
+
+            [1] => Array
+                (
+                    [street] => 21 some avenue
+                    [city] => leeds
+                )
+
+        )
+
+    [summary] => this is a text description for this employee
+)
+*/
+```
---
+++ tests/YamlTest.php
@@ -0,0 +1,313 @@
+<?php
+/**
+ * OriginPHP Framework
+ * Copyright 2018 - 2019 Jamiel Sharief.
+ *
+ * Licensed under The MIT License
+ * The above copyright notice and this permission notice shall be included in all copies or substantial
+ * portions of the Software.
+ *
+ * @copyright   Copyright (c) Jamiel Sharief
+ * @link        https://www.originphp.com
+ * @license     https://opensource.org/licenses/mit-license.php MIT License
+ */
+
+namespace Origin\Test\Yaml;
+
+use Origin\Yaml\Yaml;
+use Origin\Yaml\Exception\YamlException;
+
+class YamlTest extends \PHPUnit\Framework\TestCase
+{
+    public function testFromArrayScalar()
+    {
+        $student = [
+            'id' => 1234,
+            'name' => 'james',
+            'date' => '2019-05-05',
+            'boolean' => false,
+        ];
+        $Yaml = Yaml::fromArray($student);
+        $expected = <<< EOT
+id: 1234
+name: james
+date: 2019-05-05
+boolean: false
+EOT;
+        $this->assertStringContainsString($expected, $Yaml);
+    }
+    public function testFromArrayYaml()
+    {
+        $student = [
+            'id' => 1234,
+            'address' => [
+                'line' => '458 Some Road
+                Somewhere, Something', // multi line
+                'city' => 'london',
+            ],
+            
+        ];
+        $Yaml = Yaml::fromArray($student);
+       
+        $expected = <<< EOT
+id: 1234
+address: 
+  line: | 458 Some Road
+                Somewhere, Something
+  city: london
+EOT;
+        $this->assertStringContainsString($expected, $Yaml);
+    }
+
+    public function testFromList()
+    {
+        $students = ['tony','nick'];
+        $Yaml = Yaml::fromArray($students);
+        $expected = <<< EOT
+- tony
+- nick
+EOT;
+        $this->assertStringContainsString($expected, $Yaml);
+    }
+    public function testFromChildList()
+    {
+        $students = [
+            ['name' => 'tony','phones' => ['1234-456']],
+            ['name' => 'nick','phones' => ['1234-456','456-4334']],
+        ];
+        $Yaml = Yaml::fromArray($students);
+        $expected = <<< EOT
+- name: tony
+  phones: 
+    - 1234-456
+- name: nick
+  phones: 
+    - 1234-456
+    - 456-4334
+EOT;
+
+        $this->assertStringContainsString($expected, $Yaml);
+    }
+
+    public function testFromArrayMultiYamls()
+    {
+        $students = [
+            'id' => 1234,
+            'name' => 'tony',
+            'addresess' => [
+                ['street' => '1234 some road','city' => 'london'],
+                ['street' => '546 some avenue','city' => 'london'],
+            ],
+        ];
+        $Yaml = Yaml::fromArray($students);
+        $expected = <<< EOT
+id: 1234
+name: tony
+addresess: 
+  - street: 1234 some road
+    city: london
+  - street: 546 some avenue
+    city: london
+EOT;
+        $this->assertStringContainsString($expected, $Yaml);
+    }
+
+    public function testPlainScalarMultiline()
+    {
+        $Yaml = <<< EOF
+multi:
+  a
+  b
+  c
+  d
+name: test
+EOF;
+        $this->assertEquals('a b c d', Yaml::toArray($Yaml)['multi']);
+    }
+
+    public function testFromArrayMultiLevel()
+    {
+        $data = [
+            'services' => [
+                'app' => [
+                    'build' => '.',
+                    'depends_on' => [
+                        'db',
+                    ],
+                ],
+                'memcached' => [
+                    'image' => 'memcached',
+                ],
+            ],
+            'volumes' => [
+                'mysql' => 'abc', // leaving this blank is a problem. works with docker. but cant parse it
+            ],
+        ];
+        $Yaml = Yaml::fromArray($data);
+        $expected = <<< EOT
+services: 
+  app: 
+    build: .
+    depends_on: 
+      - db
+  memcached: 
+    image: memcached
+volumes: 
+  mysql: abc
+EOT;
+        $this->assertStringContainsString($expected, $Yaml);
+    }
+
+    public function testParseValues()
+    {
+        $Yaml = <<< EOF
+enabled: true
+disabled: false
+empty: null
+EOF;
+        $result = Yaml::toArray($Yaml);
+        $this->assertEquals(true, $result['enabled']);
+        $this->assertEquals(false, $result['disabled']);
+        $this->assertNull($result['empty']);
+    }
+
+    public function testParseIndexedList()
+    {
+        $Yaml = <<< EOT
+---
+# List of fruits
+-
+  name: james
+-
+  name: amy
+EOT;
+        $expected = [['name' => 'james'],['name' => 'amy']];
+        $this->assertEquals($expected, Yaml::toArray($Yaml));
+    }
+
+    public function testParseList()
+    {
+        $Yaml = <<< EOT
+---
+# List of fruits
+fruits:
+    - Apple
+    - Orange
+    - Banana
+EOT;
+        $expected = ['fruits' => ['Apple','Orange','Banana']];
+        $this->assertEquals($expected, Yaml::toArray($Yaml));
+    }
+
+    public function testParseDictonary()
+    {
+        $Yaml = <<< EOT
+---
+# Employee record
+employee:
+    name: James
+    position: Senior Developer
+EOT;
+   
+        $expected = ['employee' => ['name' => 'James','position' => 'Senior Developer']];
+        $this->assertEquals($expected, Yaml::toArray($Yaml));
+    }
+
+    public function testParseRecordSet()
+    {
+        $Yaml = <<< EOT
+---
+# Employees 
+- 100:
+  name: James
+  position: Senior Developer
+- 200:
+  name: Tony
+  position: Manager
+
+EOT;
+        $expected = [
+            '100' => ['name' => 'James','position' => 'Senior Developer'],
+            '200' => ['name' => 'Tony','position' => 'Manager'],
+        ];
+        $this->assertEquals($expected, Yaml::toArray($Yaml));
+    }
+
+    public function testParseMultiLineBlock()
+    {
+        $Yaml = <<< EOT
+block_1: |
+            this is a multiline block
+            of text
+block_2: >
+            this also is a multiline block
+            of text
+EOT;
+        $expected = [
+            'block_1' => "this is a multiline block\nof text", // literal
+            'block_2' => 'this also is a multiline block of text', // folded
+        ];
+        $result = Yaml::toArray($Yaml);
+         
+        $this->assertSame($expected, Yaml::toArray($Yaml));
+    }
+
+    public function testComplicated()
+    {
+        $Yaml = <<< EOT
+---
+# Employee record
+name: James Anderson
+job: PHP developer
+active: true
+fruits:
+    - Apple
+    - Banana
+phones:
+    home: 0207 123 4567
+    mobile: 123 456 567
+addresses:
+    - street: 2 Some road
+      city: London
+    - street: 5 Some avenue
+      city: Manchester
+description: |
+    Lorem ipsum dolor sit amet, 
+    ea eum nihil sapientem, timeam 
+    constituto id per. 
+EOT;
+        $expected = '{"name":"James Anderson","job":"PHP developer","active":true,"fruits":["Apple","Banana"],"phones":{"home":"0207 123 4567","mobile":"123 456 567"},"addresses":[{"street":"2 Some road","city":"London"},{"street":"5 Some avenue","city":"Manchester"}],"description":"Lorem ipsum dolor sit amet,\nea eum nihil sapientem, timeam\nconstituto id per."}';
+        $this->assertEquals($expected, json_encode(Yaml::toArray($Yaml)));
+    }
+
+    public function testParseChildNumericalList()
+    {
+        $Yaml = <<< EOT
+# Employee record
+name: James
+addresses:
+    -
+      city: London
+    -
+      city: Liverpool
+EOT;
+        $expected = [['city' => 'London'],['city' => 'Liverpool']];
+        $result = Yaml::toArray($Yaml);
+                 
+        $this->assertSame($expected, $result['addresses']);
+    }
+
+    public function testUsingTabsException()
+    {
+        $this->expectException(YamlException::class);
+        $Yaml = "\tname: no tab please";
+        Yaml::toArray($Yaml);
+    }
+
+    public function testMultiDocumentStreamException()
+    {
+        $this->expectException(YamlException::class);
+        $Yaml = "...\nname: value...";
+        Yaml::toArray($Yaml);
+    }
+}
